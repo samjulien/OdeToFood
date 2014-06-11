a58ced26e726bfdf3caf8147fba14b3babd1fdd9
Sam Julien <sjulien@tradepmr.com>
2014-06-11 16:48:51 -0400

    Finished the Working with Data Module

--- OdeToFood/Controllers/ReviewsController.cs
+++ OdeToFood/Controllers/ReviewsController.cs
@@ -1,144 +1,70 @@
-﻿//using OdeToFood.Models;
-//using System;
-//using System.Collections.Generic;
-//using System.Linq;
-//using System.Web;
-//using System.Web.Mvc;
+﻿using OdeToFood.Models;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
 
-//namespace OdeToFood.Controllers
-//{
-//    public class ReviewsController : Controller
-//    {
-//        [ChildActionOnly]
-//        public ActionResult BestReview()
-//        {
-//            var bestReview = from r in _reviews
-//                             orderby r.Rating descending
-//                             select r;
-//            return PartialView("_Review", bestReview.First());
-//        }
+namespace OdeToFood.Controllers
+{
+    public class ReviewsController : Controller
+    {
+        OdeToFoodDb _db = new OdeToFoodDb();
 
-//        //
-//        // GET: /Reviews/
 
-//        public ActionResult Index()
-//        {
-//            var model =
-//                from r in _reviews
-//                orderby r.Country
-//                select r;
+        public ActionResult Index([Bind(Prefix="id")] int restaurantId)
+        {
+            var restaurant = _db.Restaurants.Find(restaurantId);
+            if (restaurant != null)
+            {
+                return View(restaurant);
+            }
+            return HttpNotFound();
+        }
 
-//            return View(model);
-//        }
+        [HttpGet]
+        public ActionResult Create(int restaurantId)
+        {
+            return View();
+        }
+        
+        [HttpPost]
+        public ActionResult Create(RestaurantReview review)
+        {
+            if (ModelState.IsValid)
+            {
+                _db.Reviews.Add(review);
+                _db.SaveChanges();
+                return RedirectToAction("Index", new { id = review.RestaurantID });
+            }
+            return View(review);
+        }
 
-//        //
-//        // GET: /Reviews/Details/5
+        [HttpGet]
+        public ActionResult Edit(int id)
+        {
+            var model = _db.Reviews.Find(id);
+            return View(model);
+        }
 
-//        public ActionResult Details(int id)
-//        {
-//            return View();
-//        }
+        [HttpPost]
+        public ActionResult Edit(RestaurantReview review)
+        {
+            if (ModelState.IsValid)
+            {
+                _db.Entry(review).State = EntityState.Modified;
+                _db.SaveChanges();
+                return RedirectToAction("Index", new { id = review.RestaurantID });
+            }
+            return View(review);
+        }
 
-//        //
-//        // GET: /Reviews/Create
+        protected override void Dispose(bool disposing)
+        {
+            _db.Dispose();
+            base.Dispose(disposing);
+        }
 
-//        public ActionResult Create()
-//        {
-//            return View();
-//        }
-
-//        //
-//        // POST: /Reviews/Create
-
-//        [HttpPost]
-//        public ActionResult Create(FormCollection collection)
-//        {
-//            try
-//            {
-//                // TODO: Add insert logic here
-
-//                return RedirectToAction("Index");
-//            }
-//            catch
-//            {
-//                return View();
-//            }
-//        }
-
-//        //
-//        // GET: /Reviews/Edit/5
-
-//        public ActionResult Edit(int id)
-//        {
-//            var review = _reviews.Single(r => r.Id == id);
-
-//            return View(review);
-//        }
-
-//        //
-//        // POST: /Reviews/Edit/5
-
-//        [HttpPost]
-//        public ActionResult Edit(int id, FormCollection collection)
-//        {
-//            var review = _reviews.Single(r => r.Id == id);
-//            if (TryUpdateModel(review))
-//            {
-//                // ..
-//                return RedirectToAction("Index");
-//            }
-//            return View(review);
-//        }
-
-//        //
-//        // GET: /Reviews/Delete/5
-
-//        public ActionResult Delete(int id)
-//        {
-//            return View();
-//        }
-
-//        //
-//        // POST: /Reviews/Delete/5
-
-//        [HttpPost]
-//        public ActionResult Delete(int id, FormCollection collection)
-//        {
-//            try
-//            {
-//                // TODO: Add delete logic here
-
-//                return RedirectToAction("Index");
-//            }
-//            catch
-//            {
-//                return View();
-//            }
-//        }
-
-//        static List<RestaurantReview> _reviews = new List<RestaurantReview>
-//        {
-//            new RestaurantReview {
-//                Id = 1,
-//                Name = "Cinnamon Club",
-//                City="London",
-//                Country="UK",
-//                Rating=10,
-//            },
-//            new RestaurantReview {
-//                Id = 2,
-//                Name = "Marrakesh",
-//                City="DC",
-//                Country="USA",
-//                Rating=10,
-//            },
-//            new RestaurantReview {
-//                Id = 3,
-//                Name = "The House of Elliot",
-//                City="Ghent",
-//                Country="Belgium",
-//                Rating=10,
-//            }
-//        };
-//    }
-//}
+    }  
+}
--- OdeToFood/Models/RestaurantReview.cs
+++ OdeToFood/Models/RestaurantReview.cs
@@ -1,17 +1,36 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Web;
 
 namespace OdeToFood.Models
 {
-    public class RestaurantReview
+    public class RestaurantReview : IValidatableObject
     {
         public int Id { get; set; }
+
+        [Required]
+        [StringLength(1024)]
         public string Body { get; set; }
+
         public int RestaurantID { get; set; }
+
+        [Range(1,10)]
+        [Required]
         public int Rating { get; set; }
+
+        [Display(Name="User Name")]
+        [DisplayFormat(NullDisplayText="anonymous")]
         public string ReviewerName { get; set; }
 
+
+        IEnumerable<ValidationResult> IValidatableObject.Validate(ValidationContext validationContext)
+        {
+            if (Rating < 2 && ReviewerName.ToLower().StartsWith("scott"))
+            {
+                yield return new ValidationResult("Sorry, Scott, you can't do this.");
+            }
+        }
     }
 }
