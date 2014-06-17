a24688d8a15f2e3cbaa618a23658808fc26d21d9
Sam Julien <sjulien@tradepmr.com>
2014-06-17 15:55:51 -0400

    Started the Test Module

---
+++ OdeToFood.Tests/Features/RestaurantRater.cs
@@ -0,0 +1,25 @@
+﻿using OdeToFood.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace OdeToFood.Tests.Features
+{
+    class RestaurantRater
+    {
+        private Restaurant _restaurant;
+
+        public RestaurantRater(Restaurant restaurant)
+        {
+            this._restaurant = restaurant;
+        }
+
+        public RatingResult ComputeRating(int numberOfReviews)
+        {
+            var result = new RatingResult();
+            result.Rating = (int)_restaurant.Reviews.Average(r => r.Rating);
+            return result;
+        }
+    }
+}
---
+++ OdeToFood.Tests/Features/UnitTest1.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using OdeToFood.Models;
+using System.Collections.Generic;
+
+// Comments
+
+namespace OdeToFood.Tests.Features
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void Computes_Result_For_One_Review()
+        {
+            var data = new Restaurant();
+            data.Reviews = new List<RestaurantReview>();
+            data.Reviews.Add(new RestaurantReview() { Rating = 4 });
+
+            var rater = new RestaurantRater(data);
+            var result = rater.ComputeRating(10);
+
+            Assert.AreEqual(4, result.Rating);
+        }
+
+        [TestMethod]
+        public void Computes_Result_For_Two_Reviews()
+        {
+            var data = new Restaurant();
+            data.Reviews = new List<RestaurantReview>();
+            data.Reviews.Add(new RestaurantReview() { Rating = 4 });
+            data.Reviews.Add(new RestaurantReview() { Rating = 8 });
+
+            var rater = new RestaurantRater(data);
+            var result = rater.ComputeRating(10);
+
+            Assert.AreEqual(6, result.Rating);
+        }
+    }
+}
